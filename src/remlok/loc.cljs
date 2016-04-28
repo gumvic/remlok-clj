ec3f5b807334fdfde522bb907c80cfbd97366aa9
Viktor Osychniuk <osyuchnyuk@interesnee.ru>
2016-04-28 21:41:24 +0300

    refactor sub, mut

--- src/remlok/loc.cljs
+++ src/remlok/loc.cljs
@@ -1,5 +1,4 @@
 (ns remlok.loc
-  (:refer-clojure :exclude [read])
   (:require
     [reagent.core :as r]
     [reagent.ratom :refer-macros [reaction] :refer [make-reaction]]
@@ -20,22 +19,30 @@
 (defn mut [attr mut]
   (swap! mutfs assoc attr mut))
 
+(defn- reactive? [x]
+  (satisfies? IDeref x))
+
+;; TODO refactor sub**, sub*** - better names
+(defn- sub** [node ctx]
+  (let [attr (get (q/node->ast node) :attr)
+        f (->> (constantly nil)
+               (get @resfs :default)
+               (get @resfs attr))
+        res (f db node ctx)
+        res (if (fn? res)
+              (make-reaction res)
+              res)]
+    [attr res]))
+
+(defn- sub*** [[attr res]]
+  (if (reactive? res)
+    [attr @res]
+    [attr res]))
+
 (defn- sub* [query ctx]
-  (let [xs (mapv
-             #(let [attr (get (q/node->ast %) :attr)
-                    f (->> (constantly nil)
-                           (get @resfs :default)
-                           (get @resfs attr))]
-               [attr (f db query ctx)])
-             query)
-        {:keys [consts reacts]} (group-by
-                                  #(if (fn? (second %)) :reacts :consts)
-                                  xs)
-        reacts (mapv #(vector (first %) (make-reaction (second %))) reacts)]
+  (let [rs (mapv #(sub** % ctx) query)]
     (reaction
-      (merge
-        consts
-        (map #(vector (first %) @(second %)) reacts)))))
+      (into {} (map sub***) rs))))
 
 (defn sub
   ([query]
@@ -43,15 +50,16 @@
   ([query ctx]
     (sub* query ctx)))
 
+;; TODO refactor mut** - better name
+(defn- mut** [db node]
+  (let [attr (get (q/node->ast node) :attr)
+        f (->> (fn [db _] db)
+               (get @mutfs :default)
+               (get @mutfs attr))]
+    (f db node)))
+
 (defn- mut* [db query]
-  (reduce
-    #(let [attr (get (q/node->ast %2) :attr)
-           f (->> (fn [db _] db)
-                  (get @mutfs :default)
-                  (get @mutfs attr))]
-      (f %1 %2))
-    db
-    query))
+  (reduce mut** db query))
 
 (defn mut! [query]
   (swap! db mut* query)
