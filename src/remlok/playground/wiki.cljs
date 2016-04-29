21dae9c27fe9e8c6e2a9520ef16fe17a80066f59
Viktor Osychniuk <osyuchnyuk@interesnee.ru>
2016-04-29 14:47:17 +0300

    add wiki example

--- src/remlok/playground/wiki.cljs
+++ src/remlok/playground/wiki.cljs
@@ -1,5 +1,55 @@
 (ns remlok.playground.wiki
+  (:import [goog Uri]
+           [goog.net Jsonp])
   (:require
-    [remlok.loc :refer [pub sub rsub mut mut!]]))
+    [remlok.loc :refer [pub sub rpub rsub mut mut!]]
+    [remlok.query :as q]))
 
-(defn root [])
+(defn wiki [s res]
+  (let [uri "http://en.wikipedia.org/w/api.php?action=opensearch&format=json&search="
+        gjsonp (Jsonp. (Uri. (str uri s)))]
+    (.send gjsonp nil (comp res second))))
+
+(pub
+  :search
+  (fn [db]
+    (fn []
+      (get @db :search))))
+
+(pub
+  :sugg
+  (fn [db]
+    (fn []
+      (get @db :sugg))))
+
+(rpub
+  :sugg
+  (fn [db node]
+    node))
+
+(mut
+  :search
+  (fn [db node]
+    (let [args (get (q/node->ast node) :args)]
+      (assoc db :search args))))
+
+(defn input [search]
+  [:input
+   {:on-change #(mut! `[(:search ~(-> % .-target .-value))])
+    :value (str search)}])
+
+(defn list [sugg]
+  [:ul
+   (map
+     (fn [s]
+       [:li (str s)])
+     sugg)])
+
+(defn root []
+  (let [search (sub [:search])
+        sugg ()]
+    (fn []
+      (let [{:keys [search sugg]} @props]
+        [:div
+         [input search]
+         [list sugg]]))))
