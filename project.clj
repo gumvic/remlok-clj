f94306f9e77f434379824fc2fae3c2b816cc6479
Viktor Osychniuk <osyuchnyuk@interesnee.ru>
2016-05-17 18:19:26 +0300

    0.1.0

--- project.clj
+++ project.clj
@@ -6,4 +6,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.7.228"]
                  [reagent "0.5.1"]]
+  :profiles {:debug {:debug true}
+             :dev   {:plugins [[lein-cljsbuild "1.1.2"]
+                               [lein-figwheel "0.5.3-1"]]}}
   :source-paths ["src"])
