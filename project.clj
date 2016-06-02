67de819f3d4f2e2b6472a18f85aed9ea5cf595ff
Viktor Osychniuk <osyuchnyuk@interesnee.ru>
2016-06-02 14:44:54 +0300

    description in now enthusiastic

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject gumvic/remlok "0.3.0"
-  :description "Miniature Clojure(Script) UI framework with optimistic updates and all that."
+  :description "Miniature Clojure(Script) UI framework with optimistic updates and all that!"
   :url "https://github.com/gumvic/remlok"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
